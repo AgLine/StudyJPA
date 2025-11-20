7fcf5dbd62b6cec57a4997b8b4dad6154a0fd82b
agline <rladmstjs120@gmail.com>
2025-11-20 21:48:00 +0900

    데이터변경은 트랜잭션 안에서

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,7 +8,19 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
+
+        //데이터를 변경하는 모든 작업은 트랜잭션 안에서 해야함
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Member member = new Member();
+
+        member.setId(1L);
+        member.setName("HelloA");
+
+        em.persist(member);
+
+        tx.commit();
 
         em.close();
         emf.close();
