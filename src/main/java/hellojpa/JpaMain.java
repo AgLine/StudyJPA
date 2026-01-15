66792f313b5185348cc539f4769370dab9e2b2bc
agline <rladmstjs120@gmail.com>
2026-01-16 00:58:54 +0900

    11강

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -77,27 +77,11 @@ public class JpaMain {
 */
         //1차캐시
         try {
-            /*
-            //비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("cash");
 
-            //영속
-            System.out.println("===BEFORE===");
+            Member member = new Member(200L, "member200");
             em.persist(member);
-            System.out.println("===AFTER===");
 
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember = "+ findMember.getId());
-            System.out.println("findMember = "+ findMember.getName());
-            */
-
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
-
-            //em.persist(member);
+            em.flush();
 
             System.out.println("===============");
             tx.commit();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,17 +3,21 @@ package hellojpa;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity//JPA가 관리하는 클래스이다
 public class Member {
 
-    @Id
+    @Id//PK(Primary Key)를 지정 (필수)
     private Long id;
-    private String name;
 
-    public Member(){
+    private String name;//일반 컬럼으로 매핑됨
+
+    //JPA 표준에 따른 기본 생성자
+    //기본 생성자 접근 제어: 외부에서 아무런 값 없이 객체를 생성하는 것을 막기 위해 protected Member() {} 정도로 선언하는 것이 더 안전
+    protected Member(){
 
     }
 
+    //실제 개발 시 편리함을 위한 생성자
     public Member(long id, String name) {
         this.id =id;
         this.name = name;
